ba829161f8d198284433174276a5d00518b91755
Mark Holden <mark.holden@monscierge.com>
2022-04-10 16:36:10 -0500

    Get rules from a file rather than hardcoding them

--- Farkle/Rules.cpp
+++ Farkle/Rules.cpp
@@ -1,1 +1,20 @@
+#pragma once
 #include "Rules.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void Rules::Display() {
+    string line;
+    ifstream rulesFile;
+    rulesFile.open("..\\FarkleRules.txt");
+    if (rulesFile.is_open()) {
+        while (getline(rulesFile, line))
+        {
+            cout << line << endl;
+        }
+        rulesFile.close();
+    }
+}
